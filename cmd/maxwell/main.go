4f5805fb3f6bf484d8d904a957b106f95a9a32e8
yihhao wang <2633565580@qq.com>
2024-03-29 18:10:28 +0800

    feat(cmd): add maxwell server cmd

--- cmd/maxwell/main.go
+++ cmd/maxwell/main.go
@@ -1,11 +1,71 @@
-package maxwell
+package main
 
-var (
-	AppName   string
-	Version   string
-	BuildTime string
+import (
+	"context"
+	"fmt"
+	"github.com/dstgo/contrib/util/cfgx"
+	"github.com/dstgo/maxwell/app"
+	"github.com/dstgo/maxwell/app/conf"
+	"github.com/spf13/cobra"
+	"log/slog"
+	"os/signal"
+	"syscall"
 )
 
-func main() {
+var (
+	Version    string
+	BuildTime  string
+	ConfigFile string
+)
 
+var rootCmd = &cobra.Command{
+	Use:          "maxwell [commands] [-flags]",
+	Short:        "maxwell is a opensource personal don‘t starve together web panel",
+	Long:         "maxwell is a opensource personal don‘t starve together web panel",
+	SilenceUsage: true,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		// load config file
+		var appconf conf.AppConf
+		if err := cfgx.LoadConfigAndMapTo(ConfigFile, &appconf); err != nil {
+			return err
+		}
+		appconf.Version = Version
+		appconf.BuildTime = BuildTime
+
+		// register os signal listener
+		ctx := context.Background()
+		notifyContext, cancelFunc := signal.NotifyContext(ctx, syscall.SIGINT, syscall.SIGKILL, syscall.SIGTERM)
+		defer cancelFunc()
+
+		// initialize app
+		server, err := app.NewApp(notifyContext, &appconf)
+		if err != nil {
+			return err
+		}
+
+		done := make(chan struct{})
+		go func() {
+			slog.Info(fmt.Sprintf("server is listening at %s", appconf.Server.Address))
+			server.ListenAndServe()
+			done <- struct{}{}
+			close(done)
+		}()
+
+		select {
+		case <-notifyContext.Done():
+			slog.InfoContext(ctx, "received os signal, ready to shutdown")
+		case <-done:
+			slog.InfoContext(ctx, "shutdown")
+		}
+		server.Shutdown(ctx)
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.PersistentFlags().StringVarP(&ConfigFile, "config", "f", "conf.yaml", "app configuration file")
+}
+
+func main() {
+	rootCmd.Execute()
 }
