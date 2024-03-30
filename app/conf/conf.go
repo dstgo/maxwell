e44c74c79bd164509084328a544ee19ae87877b2
yihhao wang <2633565580@qq.com>
2024-03-30 20:57:07 +0800

    feat(conf): update app conf

--- app/conf/conf.go
+++ app/conf/conf.go
@@ -7,9 +7,10 @@ import (
 )
 
 type AppConf struct {
-	DB     gorms.Config `mapstructure:"db"`
+	Server ServerConf   `mapstructure:"server"`
 	Log    logx.Options `mapstructure:"log"`
-	Server *ServerConf  `mapstructure:"server"`
+	DB     gorms.Config `mapstructure:"db"`
+	Redis  RedisConf    `mapstructure:"redis"`
 
 	Version   string
 	BuildTime string
@@ -21,4 +22,24 @@ type ServerConf struct {
 	WriteTimeout time.Duration `mapstructure:"writeTimeout"`
 	IdleTimeout  time.Duration `mapstructure:"idleTimeout"`
 	MultipartMax int64         `mapstructure:"multipartMax"`
+}
+
+type RedisConf struct {
+	Address      string        `mapstructure:"address"`
+	Password     string        `mapstructure:"password"`
+	WriteTimeout time.Duration `mapstructure:"writeTimeout"`
+	ReadTimeout  time.Duration `mapstructure:"readTimeout"`
+}
+
+type JwtConf struct {
+	Issuer string `mapstructure:"issuer"`
+	Access struct {
+		Expire time.Duration `mapstructure:"expire"`
+		Delay  time.Duration `mapstructure:"delay"`
+		Key    string        `mapstructure:"key"`
+	} `mapstructure:"access"`
+	Refresh struct {
+		Expire time.Duration `mapstructure:"expire"`
+		Key    string        `mapstructure:"key"`
+	} `mapstructure:"refresh"`
 }
