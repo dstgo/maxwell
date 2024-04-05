3eaff68d6aae540cae34610962625df35807d13b
yihhao wang <2633565580@qq.com>
2024-04-05 22:08:25 +0800

    feat(api): add swag go generate directives

--- app/api/api.go
+++ app/api/api.go
@@ -2,6 +2,20 @@ package api
 
 import "github.com/dstgo/maxwell/app/types"
 
-func RegisterRouter(env types.Env) {
+// RegisterRouter
+// @title	                        MaxWell HTTP API
+// @version		                    v0.0.0Beta
+// @description                     This is maxwell swagger generated api documentation, know more information about maxwell on GitHub.
+// @contact.name                    dstgo
+// @contact.url                     https://github.com/dstgo/maxwell
+// @BasePath	                    /api/
+// @license.name                    MIT LICENSE
+// @license.url                     https://mit-license.org/
+// @securityDefinitions.apikey      BearerAuth
+// @in                              header
+// @name                            Authorization
+//
+//go:generate swag init --ot yaml --generatedTime --instanceName "maxwell" -g api.go -d ./,../types --output ./docs && swag fmt -g api.go -d ./
+func RegisterRouter(env *types.Env) {
 
 }
