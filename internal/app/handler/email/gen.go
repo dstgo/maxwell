60a58333909c6a9fb9d08ca4bb79a0cc64fca4ad
yihhao wang <2633565580@qq.com>
2024-04-17 21:39:10 +0800

    feat(email): add confirm email template

---
+++ internal/app/handler/email/gen.go
@@ -0,0 +1,27 @@
+package email
+
+import (
+	"fmt"
+	"github.com/matcornic/hermes/v2"
+	"time"
+)
+
+func ConfirmCodeMail(usage, name, code string, ttl time.Duration) hermes.Email {
+	return hermes.Email{
+		Body: hermes.Body{
+			Name: name,
+			Intros: []string{
+				fmt.Sprintf("Welcome, you are applying for a verification code to %s.", usage),
+			},
+			Actions: []hermes.Action{
+				{
+					Instructions: fmt.Sprintf("Please use your code as quickly, it will be expired in %s.", ttl.String()),
+					InviteCode:   code,
+				},
+			},
+			Outros: []string{
+				"If it is not your own operation, please ignore this email. If you need help, access https://github.com/dstgo for more information.",
+			},
+		},
+	}
+}
