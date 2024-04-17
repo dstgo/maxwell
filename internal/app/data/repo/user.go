86cd69bba6b7348390898a384aa9db2ebe782c6b
yihhao wang <2633565580@qq.com>
2024-04-17 21:36:02 +0800

    feat(repo): add user repo

---
+++ internal/app/data/repo/user.go
@@ -0,0 +1,59 @@
+package repo
+
+import (
+	"github.com/dstgo/maxwell/ent"
+	"github.com/dstgo/maxwell/ent/user"
+	"golang.org/x/net/context"
+)
+
+func NewUserRepo(client *ent.Client) *UserRepo {
+	return &UserRepo{Ent: client}
+}
+
+type UserRepo struct {
+	Ent *ent.Client
+}
+
+// FindByNameOrMail returns a User matching the given name or email
+func (u *UserRepo) FindByNameOrMail(ctx context.Context, name string) (*ent.User, error) {
+	return u.Ent.User.Query().
+		Where(
+			user.Or(
+				user.UsernameEQ(name),
+				user.EmailEQ(name),
+			),
+		).Only(ctx)
+}
+
+// FindByName returns a user matching the given name
+func (u *UserRepo) FindByName(ctx context.Context, name string) (*ent.User, error) {
+	return u.Ent.User.Query().
+		Where(
+			user.UsernameEQ(name),
+		).Only(ctx)
+}
+
+// FindByEmail returns a User matching the given email
+func (u *UserRepo) FindByEmail(ctx context.Context, email string) (*ent.User, error) {
+	return u.Ent.User.Query().
+		Where(
+			user.EmailEQ(email),
+		).Only(ctx)
+}
+
+// CreateNewUser creates a new user with the minimum information
+func (u *UserRepo) CreateNewUser(ctx context.Context, username string, email string, password string) (*ent.User, error) {
+	return u.Ent.User.Create().
+		SetUsername(username).
+		SetEmail(email).
+		SetPassword(password).
+		Save(ctx)
+}
+
+// UpdatePassword update the user password with specified email
+func (u *UserRepo) UpdatePassword(ctx context.Context, email string, password string) (int, error) {
+	return u.Ent.User.Update().
+		Where(user.PasswordEQ(email)).
+		SetPassword(password).
+		Save(ctx)
+}
